af84fe1f1ac3c44188a2edc286eb0ce19a1b68ca
Utharn Buranasaksee <utarn@yahoo.com>
2021-08-14 10:19:56 +0700

    Add project files.

---
+++ MvcDay1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvcDay1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ MvcDay1/Models/EmailSettingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcDay1.Models
+{
+    // https://myaccount.google.com/lesssecureapps
+    public class EmailSettingModel
+    {
+        public string Host { get; set; } = default!;
+        public int Port { get; set; } = default!;
+        public string User { get; set; } = default!;
+        public string Password{ get; set; } = default!;
+    }
+}
